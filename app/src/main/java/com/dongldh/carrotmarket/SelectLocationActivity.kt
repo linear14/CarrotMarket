6384b0c846b68e220d4d7cc9d7e4634be1126b8e
linear14 <linear14@naver.com>
2020-04-16 16:52:41 +0900

    feat: add function : search location
    - SelectLocationActivity
    - when text changed, locations that contain those words remain on recyclerView.

--- app/src/main/java/com/dongldh/carrotmarket/SelectLocationActivity.kt
+++ app/src/main/java/com/dongldh/carrotmarket/SelectLocationActivity.kt
@@ -2,9 +2,12 @@ package com.dongldh.carrotmarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dongldh.carrotmarket.Database.DBHelper
@@ -15,15 +18,47 @@ import kotlinx.android.synthetic.main.item_select_location.view.*
 
 class SelectLocationActivity : AppCompatActivity() {
 
+    // 전체 지역 리스트
     val locationList = mutableListOf<DataLocation>()
+    // 검색창에 적힌 글자가 포함된 지역 리스트
+    val searchedLocationList = mutableListOf<DataLocation>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_location)
 
         LocationRecyclerSetting()
+
+        clear_image.setOnClickListener { search_input.text.clear() }
+
+        // 검색 창 글자 바뀔 때 마다 포함된 글자를 담는 리스트를 리사이클러뷰에 뿌려줄 수 있도록 설정
+        search_input.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                searchedLocationList.clear()
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if(p0!!.length> 0) clear_image.visibility = View.VISIBLE
+                else clear_image.visibility = View.GONE
+
+                for(i in locationList) {
+                    if(i.name.contains(p0)) searchedLocationList.add(i)
+                }
+
+                result_recycler.adapter = ResultLocationAdapter(searchedLocationList)
+
+                // 검색 결과가 없다면? -> '결과가 없습니다' TextView를 보여주자.
+                if(searchedLocationList.isEmpty()) no_result_text.visibility = View.VISIBLE
+                else no_result_text.visibility = View.GONE
+            }
+
+        })
     }
 
-    // 전체 location을 이름 오름차순으로 recyclerview에 뿌려주는 역할을 하는 메서드
+    // 전체 location에 대하여 이름 오름차순으로 recyclerview에 뿌려주는 역할을 하는 메서드
     private fun LocationRecyclerSetting() {
         val helper = DBHelper(this)
         val db = helper.writableDatabase
