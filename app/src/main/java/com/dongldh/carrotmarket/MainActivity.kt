38a25351b38c4a63f9f84148cf029981bb47ecd6
linear14 <linear14@naver.com>
2020-04-19 17:34:13 +0900

    feat: fragment transitions on MainActivity

--- app/src/main/java/com/dongldh/carrotmarket/MainActivity.kt
+++ app/src/main/java/com/dongldh/carrotmarket/MainActivity.kt
@@ -2,11 +2,91 @@ package com.dongldh.carrotmarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import com.dongldh.carrotmarket.MainFragment.CategoryFragment
+import com.dongldh.carrotmarket.MainFragment.ChatFragment
+import com.dongldh.carrotmarket.MainFragment.HomeFragment
+import com.dongldh.carrotmarket.MainFragment.MyCarrotFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+        bottom_navigation.selectedItemId = R.id.action_home
+    }
+
+    // BottomNavigation 선택시 화면 전환
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_home -> {
+                val homeFragment = HomeFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, homeFragment)
+                    .commit()
+
+                title_text.text = "내 동네"
+                selected_location_image.visibility = View.VISIBLE
+                search_image.visibility = View.VISIBLE
+                location_setting_image.visibility = View.VISIBLE
+                alarm_image.visibility = View.VISIBLE
+                setting_image.visibility= View.GONE
+
+                return true
+            }
+
+            R.id.action_category -> {
+                val categoryFragment = CategoryFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, categoryFragment).commit()
+
+                title_text.text = "카테고리"
+                selected_location_image.visibility = View.GONE
+                search_image.visibility = View.VISIBLE
+                location_setting_image.visibility = View.GONE
+                alarm_image.visibility = View.VISIBLE
+                setting_image.visibility= View.GONE
+
+                return true
+            }
+
+            R.id.action_write -> {
+
+                return true
+            }
+
+            R.id.action_chat -> {
+                val chatFragment = ChatFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, chatFragment).commit()
+
+                title_text.text = "채팅"
+                selected_location_image.visibility = View.GONE
+                search_image.visibility = View.GONE
+                location_setting_image.visibility = View.GONE
+                alarm_image.visibility = View.GONE
+                setting_image.visibility= View.GONE
+
+                return true
+            }
+
+            R.id.action_my_carrot -> {
+                val myCarrotFragment = MyCarrotFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, myCarrotFragment).commit()
+
+                title_text.text = "나의 당근"
+                selected_location_image.visibility = View.GONE
+                search_image.visibility = View.GONE
+                location_setting_image.visibility = View.GONE
+                alarm_image.visibility = View.GONE
+                setting_image.visibility= View.VISIBLE
+
+                return true
+            }
+        }
+        return false
     }
 }
---
+++ app/src/main/java/com/dongldh/carrotmarket/MainFragment/MyCarrotFragment.kt
@@ -0,0 +1,15 @@
+package com.dongldh.carrotmarket.MainFragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dongldh.carrotmarket.R
+
+class MyCarrotFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_my_carrot, container, false)
+        return view
+    }
+}
