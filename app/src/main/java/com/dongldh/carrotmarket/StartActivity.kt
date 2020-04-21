b6c6613d08d66f6e632b10b9d3b1652dba50764f
linear14 <47495592+linear14@users.noreply.github.com>
2020-04-21 16:39:43 +0900

    feat: starting activity depends on location preference
    - when sharedpreference location has null value -> StartActivity
    - else? -> MainActivity

--- app/src/main/java/com/dongldh/carrotmarket/StartActivity.kt
+++ app/src/main/java/com/dongldh/carrotmarket/StartActivity.kt
@@ -1,17 +1,23 @@
 package com.dongldh.carrotmarket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_start.*
 
 class StartActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
 
-        // ImageView 안의 gif가 움직일 수 있도록 설정하자 (Glide 라이브러리 이용)
-        Glide.with(this).load(R.drawable.rabbit).centerCrop().into(rabbit_image)
+        if(App.preference.location != null) {
+            val intent = Intent(this@StartActivity, MainActivity::class.java)
+            startActivity(intent)
+        } else {
+            setContentView(R.layout.activity_start)
+
+            // ImageView 안의 gif가 움직일 수 있도록 설정하자 (Glide 라이브러리 이용)
+            Glide.with(this).load(R.drawable.rabbit).centerCrop().into(rabbit_image)
+        }
     }
 }
