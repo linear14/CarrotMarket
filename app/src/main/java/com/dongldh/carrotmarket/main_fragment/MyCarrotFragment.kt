ba9b59db8a0b84a6bb5e51c15cf837361b1365cd
linear14 <47495592+linear14@users.noreply.github.com>
2020-04-23 15:34:49 +0900

    feat: add function - sign out
    - create some layout for signing out

--- app/src/main/java/com/dongldh/carrotmarket/main_fragment/MyCarrotFragment.kt
+++ app/src/main/java/com/dongldh/carrotmarket/main_fragment/MyCarrotFragment.kt
@@ -1,15 +1,31 @@
 package com.dongldh.carrotmarket.main_fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.dongldh.carrotmarket.R
+import com.dongldh.carrotmarket.setting.SettingMainActivity
+import kotlinx.android.synthetic.main.fragment_my_carrot.*
+import kotlinx.android.synthetic.main.fragment_my_carrot.view.*
 
-class MyCarrotFragment: Fragment() {
+class MyCarrotFragment: Fragment(), View.OnClickListener {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_my_carrot, container, false)
+
+        view.app_setting_layout.setOnClickListener(this)
         return view
+    }
+
+    override fun onClick(v: View?) {
+        when(v) {
+            // 앱 설정 액티비티로 이동
+            app_setting_layout -> {
+                val intent = Intent(activity, SettingMainActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/dongldh/carrotmarket/setting/SettingMainActivity.kt
@@ -0,0 +1,52 @@
+package com.dongldh.carrotmarket.setting
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.dongldh.carrotmarket.App
+import com.dongldh.carrotmarket.R
+import com.dongldh.carrotmarket.StartActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_setting_main.*
+
+class SettingMainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_main)
+
+        back_image.setOnClickListener(this)
+        sign_out_text.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v) {
+            back_image -> {
+                finish()
+            }
+
+            // 로그아웃 관련된 정보
+            sign_out_text -> {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle(R.string.logout_dialog_title)
+                builder.setMessage(R.string.logout_dialog_message)
+                builder.setNeutralButton(R.string.logout_dialog_neutral) { dialog, which ->
+                    dialog.dismiss()
+                }
+                builder.setPositiveButton(R.string.logout_dialog_positive) { dialog, which ->
+                    FirebaseAuth.getInstance().signOut()
+                    dialog.dismiss()
+                    App.preference.location = null
+
+                    val intent = Intent(this, StartActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                }
+                val dialog = builder.create()
+                dialog.show()
+            }
+        }
+    }
+}
