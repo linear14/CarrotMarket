830f2e8224c259afcaf4596160114aee42090371
linear14 <47495592+linear14@users.noreply.github.com>
2020-04-26 16:54:51 +0900

    feat: layout design _ activity_write_used.xml

---
+++ app/src/main/java/com/dongldh/carrotmarket/write/WriteUsedActivity.kt
@@ -0,0 +1,13 @@
+package com.dongldh.carrotmarket.write
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dongldh.carrotmarket.R
+
+class WriteUsedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write_used)
+    }
+}
